2b2448a8595c70033e991f682c3f82506d4c29a9
Aditya <adityabasak7031@gmail.com>
2025-02-27 17:26:33 +0530

    create signup API

--- apps/http-backend/src/controllers/signup.controller.ts
+++ apps/http-backend/src/controllers/signup.controller.ts
@@ -1,21 +1,64 @@
 import { Request, Response } from "express";
 import { signupSchema } from "@repo/common/types"
+import { prismaClient } from "@repo/db/prisma"
+import bcrypt from "bcryptjs"
+
+const signup = async (req: Request, res: Response) => {
 
 
-const signup = (req: Request, res: Response) => {
+    try {
+        const parseData = signupSchema.safeParse(req.body);
 
-    const data = signupSchema.safeParse(req.body);
+        if (!parseData.success) {
+            res.status(403).json({
+                message: "Invalid inputs",
+                errors: parseData.error.errors
+            });
+            return;
+        }
 
-    if (!data.success) {
-        res.status(403).json({
-            message: "Invalid inputs"
+        const { name, username, password, photo } = parseData.data
+
+        const existingUser = await prismaClient.user.findUnique({
+            where: { username }
+        });
+
+        if (existingUser) {
+            res.status(404).json({
+                message: "user already exsits"
+            });
+            return
+        }
+
+        const slat = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, slat)
+
+        const userData = await prismaClient.user.create({
+            data: {
+                username,
+                name,
+                password: hashedPassword,
+                photo
+            }
+
+        });
+
+        res.status(201).json({
+            message: "user created successfylly",
+            user: {
+                id: userData.id,
+                name: userData.name,
+            }
+        })
+
+    } catch (err) {
+        const error = err as Error;
+        res.status(500).json({
+            message: "Internal server error",
+            err: error
         });
         return;
     }
-
-    res.send({
-        message: "Hi there"
-    })
 
 }
 export default signup;
