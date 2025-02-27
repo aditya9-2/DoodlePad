a286735a46129d9cb52cba66e61eccc4385cfdb3
Aditya <adityabasak7031@gmail.com>
2025-02-27 18:28:16 +0530

    add room route

--- apps/http-backend/src/routes/userRouter.ts
+++ apps/http-backend/src/routes/userRouter.ts
@@ -1,11 +1,13 @@
 import express, { Router } from "express";
 import signup from "../controllers/signup.controller";
 import signin from "../controllers/signin.controller";
+import room from "../controllers/room.controller";
 
 
 const router: Router = express.Router();
 router.post("/signup", signup);
 router.post("/signin", signin);
+router.post("/room", room);
 
 
 
