f56ba4b2153c12679868ff6e24d5540d43f1529d
Aditya <adityabasak7031@gmail.com>
2025-02-27 22:22:53 +0530

    add custom request

--- apps/http-backend/src/controllers/room.controller.ts
+++ apps/http-backend/src/controllers/room.controller.ts
@@ -1,15 +1,14 @@
 import { roomSchema } from "@repo/common/types"
 import { prismaClient } from "@repo/db/prisma";
-import { Request, Response } from "express"
+import { Response } from "express"
+import { CustomRequest } from "../middleware/authMiddleware";
 
-export interface CustomRequest extends Request {
-    userId?: string
-}
+
 const room = async (req: CustomRequest, res: Response) => {
 
-    const paresedData = roomSchema.safeParse(req.body);
+    const parsedData = roomSchema.safeParse(req.body);
 
-    if (!paresedData.success) {
+    if (!parsedData.success) {
         res.status(403).json({
             message: "invalid inputs",
         });
@@ -17,28 +16,34 @@ const room = async (req: CustomRequest, res: Response) => {
     }
 
     try {
-
         const userId = req.userId;
 
         if (!userId) {
-            res.status(400).json({
-                message: "User ID is required",
-            });
+            res.status(403).json({
+                message: "Unauthorized"
+            })
             return;
         }
 
-        await prismaClient.room.create({
+        const room = await prismaClient.room.create({
             data: {
-                slug: paresedData.data.name,
+                slug: parsedData.data.name,
                 adminId: userId
             }
+        })
+        res.status(201).json({
+            message: "room created successfully",
+            roomId: room.id
         });
 
-    } catch (err: any) {
+
+    } catch (err) {
+        const error = err as Error
         res.status(500).json({
             message: "Internal server error",
-            error: err.message
+            error: error.message
         });
+        return
     }
 
 
