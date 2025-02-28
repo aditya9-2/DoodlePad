e3e7a6bf68df6255a4a984571a401d415fc44b04
Aditya <adityabasak7031@gmail.com>
2025-02-28 16:43:07 +0530

    create chat route

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import userRouter from "./routes/userRouter"
+import chatRouter from "./routes/chatRouter"
 
 const app = express();
 const port = 8000
@@ -7,6 +8,7 @@ const port = 8000
 app.use(express.json());
 
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/chats", chatRouter);
 
 
 app.listen(port, () => {
