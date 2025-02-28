b4931cbb18bea54faa856cfdf77b77b5c78b184e
Aditya <adityabasak7031@gmail.com>
2025-02-28 20:41:30 +0530

    add universal cors

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import userRouter from "./routes/userRouter"
 import chatRouter from "./routes/chatRouter"
+import cors from "cors"
 
 const app = express();
 const port = 8000
 
 app.use(express.json());
-
+app.use(cors({ origin: "*" }))
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/chats", chatRouter);
 
