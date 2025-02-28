f16576bc56babc17667567129391f95f32b44267
Aditya <adityabasak7031@gmail.com>
2025-02-28 16:18:13 +0530

    add chat history route

--- apps/http-backend/src/routes/userRouter.ts
+++ apps/http-backend/src/routes/userRouter.ts
@@ -3,12 +3,14 @@ import signup from "../controllers/signup.controller";
 import signin from "../controllers/signin.controller";
 import room from "../controllers/room.controller";
 import userAuth from "../middleware/authMiddleware";
+import chatHistory from "../controllers/chatHistory.controller";
 
 
 const router: Router = express.Router();
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.post("/room", userAuth, room);
+router.get("/chat/:roomId", userAuth, chatHistory);
 
 
 
