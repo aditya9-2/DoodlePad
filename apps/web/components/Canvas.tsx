b3247285ec04721eced71885f4ee116fca692b2f
Aditya <adityabasak7031@gmail.com>
2025-03-04 16:33:11 +0530

    rendered inconButton component

--- apps/web/components/Canvas.tsx
+++ apps/web/components/Canvas.tsx
@@ -1,5 +1,9 @@
 import { initDraw } from "@/app/draw";
 import { useEffect, useRef, useState } from "react";
+import IconButton from "./IconButton";
+import { Circle, Pencil, RectangleHorizontalIcon, } from "lucide-react";
+
+type shape = "circle" | "rect" | "pencil";
 
 export function Canvas({
     roomId,
@@ -13,6 +17,7 @@ export function Canvas({
         width: window.innerWidth,
         height: window.innerHeight
     });
+    const [selectedTool, setSelectedTool] = useState<shape>("circle")
 
 
     const updateSize = () => {
@@ -38,6 +43,7 @@ export function Canvas({
 
     return (
         <div className="h-screen overflow-hidden">
+            <IconTopBar selectedTool={selectedTool} setSelectedTool={setSelectedTool} />
             <canvas
                 ref={canvasRef}
                 width={dimentions.width}
@@ -45,4 +51,34 @@ export function Canvas({
             ></canvas>
         </div>
     )
+}
+
+function IconTopBar({ selectedTool, setSelectedTool }: {
+    selectedTool: shape;
+    setSelectedTool: (s: shape) => void;
+}) {
+    return (
+        <div className="w-44 h-14 md:w-96 bg-slate-600 text-white fixed top-3 left-28 md:left-[36rem] rounded-md py-2">
+            <div className="flex gap-2 items-center justify-center">
+                <IconButton
+                    icon={<Circle />}
+                    onClick={() => { setSelectedTool("circle") }}
+                    isActive={selectedTool === "circle"}
+                />
+                <IconButton
+                    icon={<Pencil />}
+                    onClick={() => { setSelectedTool("pencil") }}
+                    isActive={selectedTool === "pencil"}
+                />
+                <IconButton
+                    icon={<RectangleHorizontalIcon />}
+                    onClick={() => { setSelectedTool("rect") }}
+                    isActive={selectedTool === "rect"}
+                />
+            </div>
+
+        </div>
+
+    )
+
 }
