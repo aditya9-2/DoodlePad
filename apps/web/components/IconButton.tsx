de3f0d78b5299bbfa726351d014f572b1ba65f90
Aditya <adityabasak7031@gmail.com>
2025-03-08 18:33:23 +0530

    made active color to green

--- apps/web/components/IconButton.tsx
+++ apps/web/components/IconButton.tsx
@@ -7,7 +7,7 @@ interface inconBtnProps {
 
 const IconButton = ({ icon, onClick, isActive }: inconBtnProps) => {
     return (
-        <div className={`cursor-pointer rounded-xl border-[1px] p-2 border-gray-400 bg-black hover:opacity-70 ${isActive ? "text-blue-800" : "text-white"}`} onClick={onClick}>{icon}</div>
+        <div className={`cursor-pointer rounded-xl border-[1px] p-2 border-gray-400 bg-black hover:opacity-70 ${isActive ? "text-green-600" : "text-white"}`} onClick={onClick}>{icon}</div>
     )
 }
 
