bc05384b0d2f3a281e334275d34b6956c600ef2b
Aditya <adityabasak7031@gmail.com>
2025-03-14 00:09:26 +0530

    add ID in every canvas types

--- apps/web/app/draw/types.ts
+++ apps/web/app/draw/types.ts
@@ -1,36 +1,43 @@
 export type canvasShapes = {
+    id: string;
     type: "rect";
     width: number;
     height: number;
     X: number;
     Y: number;
 } | {
+    id: string;
     type: "circle";
     centerX: number;
     centerY: number;
     radius: number;
 } | {
+    id: string;
     type: "pencil";
     path: { x: number; y: number }[]; // array of points
 } | {
+    id: string;
     type: "line";
     startX: number;
     startY: number;
     endX: number;
     endY: number;
 } | {
+    id: string;
     type: "arrow";
     startX: number;
     startY: number;
     endX: number;
     endY: number;
 } | {
+    id: string;
     type: "erase";
     x: number;
     y: number;
     width: number;
     height: number;
 } | {
+    id: string;
 
     type: "text";
     text: string;
