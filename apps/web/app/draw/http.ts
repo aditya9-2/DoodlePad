c708b21c7524b92209f41aaa160c642d9b472400
Aditya <adityabasak7031@gmail.com>
2025-03-15 13:05:15 +0530

    filter invalid shapes

--- apps/web/app/draw/http.ts
+++ apps/web/app/draw/http.ts
@@ -34,18 +34,46 @@ export const getExisingCanvas = async (roomId: string) => {
         console.log(`token not found in /draw/index`);
     }
 
-    const response = await axios.get(`${process.env.NEXT_PUBLIC_HTTP_URL}/api/v1/chats/${roomId}`, {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    });
+    try {
 
-    const messages = response.data.messages;
+        const response = await axios.get(`${process.env.NEXT_PUBLIC_HTTP_URL}/api/v1/chats/${roomId}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
 
-    const shapes = messages.map((shape: { message: string }) => {
-        const messageData = JSON.parse(shape.message);
-        return messageData.shape;
-    });
-    return shapes;
+        const messages = response.data.messages;
+
+        const shapes = messages.filter((message: { message: string }) => {
+            try {
+                const parsed = JSON.parse(message.message);
+                if (!parsed || !parsed.shape || !parsed.shape.id) return false;
+                // Filtering out invalid shapes
+                if (parsed.shape.type === "circle" && parsed.shape.radius === 0) return false;
+                if (parsed.shape.type === "rect" && (parsed.shape.width === 0 || parsed.shape.height === 0)) return false;
+                if (parsed.shape.type === "line" &&
+                    (parsed.shape.startX === parsed.shape.endX && parsed.shape.startY === parsed.shape.endY)) return false;
+                if (parsed.shape.type === "arrow" &&
+                    (parsed.shape.startX === parsed.shape.endX && parsed.shape.startY === parsed.shape.endY)) return false;
+                // return parsed && parsed.shape && parsed.shape.id;
+                return true;
+            } catch (err) {
+                console.log("Error parsing message: ", err);
+                return;
+            }
+        }).map((message: { message: string }) => {
+
+            const messageData = JSON.parse(message.message);
+            return messageData.shape;
+        });
+
+        return shapes;
+
+    } catch (err) {
+        console.log(`Error fetching canvas data: ${err}`);
+        return [];
+    }
+
+
 
 }
