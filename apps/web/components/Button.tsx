ae95d53f91cdbe4ffeb24b3ce21893268c75f5b4
Aditya <adityabasak7031@gmail.com>
2025-03-15 17:10:05 +0530

    add custom prop for className

--- apps/web/components/Button.tsx
+++ apps/web/components/Button.tsx
@@ -2,11 +2,12 @@ import React, { ReactNode } from 'react'
 interface buttonProps {
     label: ReactNode;
     onClick?: () => void;
+    className?: string;
 }
-const Button = ({ label, onClick }: buttonProps) => {
+const Button = ({ label, onClick, className }: buttonProps) => {
     return (
         <div>
-            <button className="p-4 py-2 text-black font-semibold bg-gradient-to-l w-[10em] from-slate-300 bg-slate-200 rounded-md"
+            <button className={`p-4 py-2 text-black font-semibold  w-[10em] bg-primary rounded-md ${className}`}
                 onClick={onClick}>{label}</button>
         </div>
     )
